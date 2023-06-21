976ef366c469b7698164361122eb53b82d92d36c
Elmar KesslerElmar Kesslergit config --global user.name Elmar <elmar.kessler@noseryoung.com>
2023-06-21 13:20:56 +0200

    Update Frontend, created File for Homepage

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -2,15 +2,6 @@ import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import ReservationPage from "./components/Reservation";
 
-function HomePage() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Willkommen auf meiner Homepage!</p>
-    </div>
-  );
-}
-
 function App() {
   return (
     <BrowserRouter>
---
+++ project/src/HomePage.tsx
@@ -0,0 +1,9 @@
+function HomePage(){
+    return(
+        <div>
+            <h1>Home</h1>
+            <p>Willkommen auf meiner Homepage!</p>
+        </div>
+    )
+}
+export default HomePage;
