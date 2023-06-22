dcdfcf266ef1eff12da38f622e26e9a4f43f7233
Elmar KesslerElmar Kesslergit config --global user.name Elmar <elmar.kessler@noseryoung.com>
2023-06-22 10:43:17 +0200

    Added MenuPage

--- project/src/HomePage.tsx
+++ project/src/HomePage.tsx
@@ -3,26 +3,12 @@ import { Link } from 'react-router-dom';
 import LogoImage from './Logo1.jpg';
 import './HomePage.css';
 import { color } from '@mui/system';
+import Navbar from './components/Navbar';
 
 function HomePage() {
   return (
     <>
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-        crossOrigin="anonymous"
-      />
-
-      <nav className="navbar fixed-top bg-body-tertiary smaller-navbar">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-          </a>
-          <button type="button" className="btn btn-outline-dark">
-            Menu
-          </button>
-        </div>
-      </nav>
+   <Navbar></Navbar>
 
       <div className="container">
         <h1 className="title" style={{color: 'white'}}>Rest-Foods</h1>
--- project/src/MenuPage.tsx
+++ project/src/MenuPage.tsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
 // import "./Menu.css";
 import MenuItem from "./components/MenuItem";
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import ResponsiveGrid from "./components/ResponsiveGrid";
+import Navbar from "./components/Navbar";
 
 function MenuPage() {
   return (
     <>
-      <MenuItem></MenuItem>
+      <Navbar></Navbar>
+      <ResponsiveGrid>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+      </ResponsiveGrid>
     </>
   );
 }
