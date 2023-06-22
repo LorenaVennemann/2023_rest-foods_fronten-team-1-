4cb8fd5cf28bf850dfec7cfe58f290ded5c569d6
Elmar KesslerElmar Kesslergit config --global user.name Elmar <elmar.kessler@noseryoung.com>
2023-06-22 08:41:27 +0200

    Added Navbar for Reservation Page

--- project/src/HomePage.tsx
+++ project/src/HomePage.tsx
@@ -1,28 +1,9 @@
-import { Link } from 'react-router-dom';
-import './HomePage.css';
+import { Link } from "react-router-dom";
+import "./HomePage.css";
 
 function HomePage() {
   return (
     <>
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-        crossOrigin="anonymous"
-      />
-
-      <nav className="navbar fixed-top bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#"></a>
-          <a href="index.html">
-            <img src="Logo.jpg" alt="Bootstrap" width="30" height="24" />
-          </a>
-          <button type="button" className="btn btn-outline-dark">
-            Dark
-          </button>
-        </div>
-      </nav>
-
       <header>
         <div className="logo"></div>
       </header>
