256b2ab9477b38526ed986be011b2be4d623da0f
LorenaVennemann <113357105+LorenaVennemann@users.noreply.github.com>
2023-06-23 11:22:20 +0200

    push_test

--- project/src/Login.tsx
+++ project/src/Login.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, createTheme, ThemeProvider } from '@mui/material';
 import { Formik, Form, Field } from 'formik';
 import login from './components/service/LoginService';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 const UserLogin = () => {
   const navigate = useNavigate();
@@ -20,48 +25,50 @@ const UserLogin = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome</h1>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        onSubmit={handleLogin}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field name="email">
-              {({ field }: { field: any }) => (
-                <TextField
-                  label="Email"
-                  type="email"
-                  {...field}
-                  fullWidth
-                  margin="normal"
-                />
-              )}
-            </Field>
-            <Field name="password">
-              {({ field }: { field: any }) => (
-                <TextField
-                  label="Password"
-                  type="password"
-                  {...field}
-                  fullWidth
-                  margin="normal"
-                />
-              )}
-            </Field>
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              Login
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div style={{ padding: '20px' }}>
+        <h1>Welcome</h1>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={handleLogin}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field name="email">
+                {({ field }: { field: any }) => (
+                  <TextField
+                    label="Email"
+                    type="email"
+                    {...field}
+                    fullWidth
+                    margin="normal"
+                  />
+                )}
+              </Field>
+              <Field name="password">
+                {({ field }: { field: any }) => (
+                  <TextField
+                    label="Password"
+                    type="password"
+                    {...field}
+                    fullWidth
+                    margin="normal"
+                  />
+                )}
+              </Field>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Login
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </ThemeProvider>
   );
 };
 
