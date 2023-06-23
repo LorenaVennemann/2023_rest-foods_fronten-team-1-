20ca4e7011ed943a7482143548e0c811ed6662b2
Elmar KesslerElmar Kesslergit config --global user.name Elmar <elmar.kessler@noseryoung.com>
2023-06-23 10:27:01 +0200

    Added Dependencies

--- project/src/KontaktPage.tsx
+++ project/src/KontaktPage.tsx
@@ -24,6 +24,9 @@ function KontaktPage() {
   <span className="contact-label">Address:</span> 123 Main Street, City, Country
 </div>
       </div>
+       {/* <div>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.413413423156!2d8.528573975892478!3d47.38436610329818!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900b9899a091c5%3A0x906525b05015723e!2sBurgermeister%20Limmatplatz!5e0!3m2!1sde!2sch!4v1687502623476!5m2!1sde!2sch" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+       </div> */}
     </>
   );
 }
