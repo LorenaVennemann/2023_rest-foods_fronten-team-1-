e82fdcc4602ff33ba61a47b3135c26e092703d2a
Elmar KesslerElmar Kesslergit config --global user.name Elmar <elmar.kessler@noseryoung.com>
2023-06-28 11:23:14 +0200

    Update Login.tsx

--- project/src/Login.tsx
+++ project/src/Login.tsx
@@ -1,94 +1,165 @@
-import { Button, TextField } from '@mui/material';
-import { Formik, Form, Field } from 'formik';
-import LoginService from './components/service/LoginService';
-import { Navigate } from 'react-router-dom'; // assuming you're using React Router
+import React from 'react';
 
-const Login = () => {
-  const handleLogin = async (values) => {
+import { useNavigate } from 'react-router-dom';
+
+import { Button, TextField } from '@mui/material';
+
+import { Formik, Form, Field } from 'formik';
+
+import login from './components/service/LoginService';
+
+
+
+
+
+const UserLogin = () => {
+
+  const navigate = useNavigate();
+
+
+
+
+  const handleLogin = async (values: { email: string; password: string }) => {
+
     const { email, password } = values;
+
     if (email && password) {
+
       console.log(`Email: ${email}, Password: ${password}`);
-      try {
-        const accessToken = await LoginService.loginUser(email, password);
-        console.log(`Access Token: ${accessToken}`);
-        localStorage.setItem('accessToken', accessToken);
-        navigate('/employee', { replace: true });
-      } catch (error) {
-        console.log(error);
-      }
+
+      const accessToken = await login().loginUser(email, password);
+
+      console.log(`Access Token: ${accessToken}`);
+
+      localStorage.setItem('accessToken', accessToken);
+
+      navigate('/employee', { replace: true });
+
     }
+
   };
+
+
+
 
   const handleBack = () => {
+
     navigate('/');
+
   };
 
+
+
+
   return (
+
     <div>
-      <h1>Any place in your app!</h1>
+
+      <h1 style={{ color: 'white' }}>Welcome</h1>
+
       <Formik
-        initialValues={{ email: 'elmar@mail.com', password: '12345' }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-            errors.email = 'Invalid email address';
-          }
-          console.log(errors);
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          handleLogin(values);
-          setSubmitting(false);
-        }}
+
+        initialValues={{ email: '', password: '' }}
+
+        onSubmit={handleLogin}
+
       >
+
         {({ isSubmitting }) => (
+
           <Form>
+
             <Field name="email">
-              {({ field }) => (
+
+              {({ field }: { field: any }) => (
+
                 <TextField
+
                   label="Email"
+
                   type="email"
+
                   {...field}
+
                   fullWidth
+
                   margin="normal"
+
                 />
+
               )}
+
             </Field>
+
             <Field name="password">
-              {({ field}) => (
+
+              {({ field }: { field: any }) => (
+
                 <TextField
+
                   label="Password"
+
                   type="password"
+
                   {...field}
+
                   fullWidth
+
                   margin="normal"
+
                 />
+
               )}
+
             </Field>
+
             <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              Login
-            </Button>
-            <br />
-            <Button
+
               variant="outlined"
-              color="secondary"
-              onClick={handleBack}
-              style={{ marginTop: '10px' }}
+
+              color="primary"
+
+              type="submit"
+
+              disabled={isSubmitting}
+
             >
-              Zurück
+
+              Login
+
             </Button>
+
+            <br />
+
+            <Button
+
+              variant="outlined"
+
+              color="secondary"
+
+              onClick={handleBack}
+
+              style={{ marginTop: '10px' }}
+
+            >
+
+              Zurück
+
+            </Button>
+
           </Form>
+
         )}
+
       </Formik>
+
     </div>
+
   );
+
 };
 
-export default Login;
+
+
+
+export default UserLogin;
